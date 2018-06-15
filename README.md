6f3aab6891f5a782f69d6612d73358bb181f63d9
Reggi Nelson <regginelson@yahoo.com>
2018-06-15 22:33:21 +0000

    Lesson 4 final commit

--- README.md
+++ README.md
@@ -4,22 +4,21 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+June 15, 2018
 
 ### Student Name:  
-(First and Last Name)
+Reggi Nelson
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+For this assignment, we are learning to create a monthly expense calculator. This will calculate our living expenses and tell us the percentage of each of those expenses.
 
 ### View Project:
-(Replace this statement with your GitHub Page URL that was created when you 
- published the project.)
+https://regginelson.github.io/lesson4_javescript/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. There are 3 rules to declare a variable. The first is that variables cannot be reserved words. Secondly, variables must start with a letter or underscore. Lastly, spaces cannot be included in the variable's name. 
+2. The two numeric data types that JavaScript accepts are integers and floating point numbers. Integers are positive and negative numbers with no decimals. And, floating-point numbers contain decimals and exponential notations. 
+3. A scripting language is a programming language that is embedded in plain text, saves bandwidth, and doesn't need to a compiler. Although, it is slower than a compiled code.
 
 
 
--- expenseCalculator.js
+++ expenseCalculator.js
@@ -36,6 +36,9 @@
 
                   var shelterAmt = document.getElementById("shelterInput").value;
                   var foodAmt = document.getElementById("foodInput").value;
+                  var householdAmt = document.getElementById("householdInput").value;
+                  var miscellaneousAmt = document.getElementById("miscellaneousInput").value;
+                  var transportationAmt = document.getElementById("transportationInput").value;
 
 
 
@@ -58,6 +61,9 @@
 
                   shelterAmt = parseFloat(shelterAmt);
                   foodAmt = parseFloat(foodAmt);
+                  householdAmt = parseFloat(householdAmt);
+                  miscellaneousAmt = parseFloat(miscellaneousAmt);
+                  transportationAmt = parseFloat(transportationAmt);
 
 
 
@@ -79,6 +85,9 @@
                   var totalExpenses; //total expenses for the user
                   var shelterPct; //shelter category percentage
                   var foodPct;  //food category percentage
+                  var householdPct;  //household category percentage
+                  var miscellaneousPct;  //miscellaneous category percentage
+                  var transportationPct;  //transportation category percentage
 
 
 
@@ -107,8 +116,12 @@
                   *******************************************************************************************************/   
 
 
+                  totalExpenses = shelterAmt + foodAmt + householdAmt + miscellaneousAmt + transportationAmt
                   shelterPct = 100 * (shelterAmt / totalExpenses);
                   foodPct = 100 * (foodAmt / totalExpenses);
+                  householdPct = 100 * (householdAmt / totalExpenses);
+                  miscellaneousPct = 100 * (miscellaneousAmt / totalExpenses);
+                  transportationPct = 100 * (transportationAmt / totalExpenses);
 
 
                   /******************************************************************************************************
@@ -135,6 +148,9 @@
                   document.getElementById("totalExpenses").innerHTML = "<b>Total Expenses:     $</b> " + totalExpenses.toFixed(2);
                   document.getElementById("shelterPercentage").innerHTML = "% for <b>shelter</b>:      " + shelterPct.toFixed(1);      
                   document.getElementById("foodPercentage").innerHTML = "% for <b>food:</b>      " + foodPct.toFixed(1);
+                  document.getElementById("householdPercentage").innerHTML = "% for <b>food:</b>      " + householdPct.toFixed(1);
+                  document.getElementById("miscellaneousPercentage").innerHTML = "% for <b>food:</b>      " + miscellaneousPct.toFixed(1);
+                  document.getElementById("transportationPercentage").innerHTML = "% for <b>food:</b>      " + transportationPct.toFixed(1);
 
 
          } //end of calculatePercentages function
@@ -171,6 +187,9 @@
                   document.getElementById("totalExpenses").innerHTML = ""; 
                   document.getElementById("shelterPercentage").innerHTML = "";      
                   document.getElementById("foodPercentage").innerHTML  = "";
+                  document.getElementById("householdPercentage").innerHTML  = "";
+                  document.getElementById("miscellaneousPercentage").innerHTML  = "";
+                  document.getElementById("transportationPercentage").innerHTML  = "";
 
 
               
